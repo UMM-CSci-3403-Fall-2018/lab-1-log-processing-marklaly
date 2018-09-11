617fc852ef251258fc5e74f51a13ecbb77f312b1
Mark Schauer <schau303@neoprene.morris.umn.edu>
2018-09-11 12:27:25 -0500

    Add line in `create_username_dist.sh` to use `wrap_contents.sh` and get tests to pass

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -2,4 +2,6 @@
 
 dir=$1
 
-cat $dir/failed_login_data.txt | awk '{ print $4 }' | sort | uniq -c | awk '{ print "data.addRow([\x27" $2 "\x27, " $1 "]);"}' > $dir/username_dist.txt
+cat $dir/*/failed_login_data.txt | awk '{ print $4 }' | sort | uniq -c | awk '{ print "data.addRow([\x27" $2 "\x27, " $1 "]);"}' > $dir/username_dist.txt
+
+bin/wrap_contents.sh $dir/username_dist.txt "html_components/username_dist" $dir/username_dist.html
