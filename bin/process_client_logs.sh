890d3c3b1b3af05b05ac9b648ba660abee471380
Mark Schauer <schau303@neoprene.morris.umn.edu>
2018-09-11 11:07:41 -0500

    Modify `process_client_logs.sh` to get the test to pass

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -4,5 +4,5 @@ dir=$1
 
 cd $dir/var/log
 
-cat * | awk '{ print $1, $2, $3, $11, cat * | awk '/Failed password for invalid user/ { print $1, $2, $3, $11, $13 } /Failed password for/ { print $1, $2, $3, $9, $11 }'$13}' | sed 's/:[[:digit:]][[:digit:]]:[[:digit:]][[:digit:]]//g' > failed_login_data.txt 
+cat * | awk '/Failed password for invalid user/ { print $1, $2, $3, $11, $13 } /Failed password for/ && !/Failed password for invalid user/ { print $1, $2, $3, $9, $11 }' | sed 's/:[[:digit:]][[:digit:]]:[[:digit:]][[:digit:]]//g' > $dir/failed_login_data.txt 
 
