5adba9e5ba05fdf73ac9abda9b3c50f17feabf33
Mark Schauer <schau303@neoprene.morris.umn.edu>
2018-09-11 10:34:43 -0500

    Modify `process_client_logs.sh` to get it almost working

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -3,4 +3,4 @@
 dir=$1
 
 cd $dir/var/log
-cat * | awk '{ print $1, $2, $3, $4, $13}' | sed 's/:[[:digit:]][[:digit:]]:[[:digit:]][[:digit:]]//g' > failed_login_data.txt 
+cat * | awk '{ print $1, $2, $3, $11, cat * | awk '/Failed password for invalid user/ { print $1, $2, $3, $11, $13 } /Failed password for/ { print $1, $2, $3, $9, $11 }'$13}' | sed 's/:[[:digit:]][[:digit:]]:[[:digit:]][[:digit:]]//g' > failed_login_data.txt 
