51817f1a6ab71491fdbaa412dc213f6b30aaa80d
Mark Schauer <schau303@neoprene.morris.umn.edu>
2018-09-13 10:07:18 -0500

    Make `create_hours_dist.sh` executable and add code to make tests pass

--- bin/create_hours_dist.sh
+++ bin/create_hours_dist.sh
@@ -2,4 +2,6 @@
 
 dir=$1
 
+cat $dir/*/failed_login_data.txt | awk '{ print $3 }' | sort | uniq -c | awk '{ print "data.addRow([\x27" $2 "\x27, " $1 "]);"}' > $dir/hours_dist.txt
 
+bin/wrap_contents.sh $dir/hours_dist.txt "html_components/hours_dist" $dir/hours_dist.html
