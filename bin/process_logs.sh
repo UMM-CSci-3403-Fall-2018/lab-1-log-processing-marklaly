6d73e3ce96063132b99838141d7167513c9dc774
Laly Xiong <xion1337@neoprene.morris.umn.edu>
2018-09-18 10:59:50 -0500

    Add code to `process_logs/sh`, but it is currently broken

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -2,4 +2,17 @@
 
 scratchdir=`mktmp -d`
 
+for f in $@
+do
+	tmpdir=`basename $f "_secure.tgz"`
+	mkdir $scratchdir/$tmpdir
+	tar -zxf $f -c $scratchdir/$tmpdir
+	bin/process_client_logs.sh $scratchdir/$tmpdir
+done
 
+bin/create_username_dist.sh $scratchdir
+bin/create_hours_dist.sh $scratchdir
+bin/create_country_dist.sh $scratchdir
+bin/assemble_report.sh $scratchdir
+
+mv $scratchdir/failed_login_summary.html
